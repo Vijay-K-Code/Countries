3e436e9595fcf3721b91aa05aa05280273088311
Vijay-K-Code <vijaysantoshkannan@gmail.com>
2025-01-14 19:16:36 -0500

    Update Country.java

--- workspace/Country.java
+++ workspace/Country.java
@@ -8,7 +8,52 @@ public class Country
 
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
  
+ublic class Country {
+    // Instance variables
+    private String name;
+    private String capital;
+    private String primaryLanguage;
+    private String imageFile;
 
+    // Default constructor
+    public Country() {
+        this.name = "";
+        this.capital = "";
+        this.primaryLanguage = "";
+        this.imageFile = "";
+    }
+
+    // Constructor with 4 arguments
+    public Country(String name, String capital, String primaryLanguage, String imageFile) {
+        this.name = name;
+        this.capital = capital;
+        this.primaryLanguage = primaryLanguage;
+        this.imageFile = imageFile;
+    }
+
+    // Accessor methods
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getPrimaryLanguage() {
+        return primaryLanguage;
+    }
+
+    public String getImageFile() {
+        return imageFile;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return name + "'s capital is " + capital + " and its primary language is " + primaryLanguage + ".";
+    }
+}
 
   
 }
